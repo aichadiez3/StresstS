24f8d40e9033f9ebd5ae730ce5ec00fef70063c8
ALvaro-REdondo <ALVAROEIVISSA13@GMAIL.COM>
2021-11-18 11:19:18 +0100

    Update Bitalino_test.java

--- src/pojos/Bitalino_test.java
+++ src/pojos/Bitalino_test.java
@@ -1,5 +1,101 @@
 package pojos;
+import java.io.Serializable;
+import java.time.LocalDate;
+public class Bitalino_test implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
-public class Bitalino_test {
+	private Integer test_id;
+	private String type;
+	private LocalDate date;
+	private String file;
+	
+	public Bitalino_test() {
+		super();
+	}
+	
+	public Bitalino_test(String type, LocalDate date, String file) {
+		super();
+		this.type = type;
+		this.date = date;
+		this.file = file;
+	}
 
+	public Integer getTest_id() {
+		return test_id;
+	}
+
+	public void setTest_id(Integer test_id) {
+		this.test_id = test_id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public String getFile() {
+		return file;
+	}
+
+	public void setFile(String file) {
+		this.file = file;
+	}
+
+	@Override
+	public String toString() {
+		return "Bitalino_test [type=" + type + ", date=" + date + ", file=" + file + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + ((file == null) ? 0 : file.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bitalino_test other = (Bitalino_test) obj;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (file == null) {
+			if (other.file != null)
+				return false;
+		} else if (!file.equals(other.file))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+	
 }
