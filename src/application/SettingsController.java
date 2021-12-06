808d1e27a82cc203c7999e4f40e7225cd619e3ea
Aïcha <a.diez22@usp.ceu.es>
2021-12-06 20:09:29 +0100

    Update SettingsController.java

--- src/application/SettingsController.java
+++ src/application/SettingsController.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -18,7 +19,7 @@ import javafx.scene.layout.Pane;
 
 public class SettingsController implements Initializable {
 
-	private Integer user_id;
+	private static Integer user_id;
 	
     @FXML
     private Pane mainPane;
@@ -37,9 +38,14 @@ public class SettingsController implements Initializable {
     
     @FXML
     private Group warning;
+    @FXML
+    private Group okayMessage;
     
     @FXML
     private Group okayVerification;
+    
+    @FXML
+    private Label warningText;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -50,22 +56,20 @@ public class SettingsController implements Initializable {
 		saveButton.setOnAction((ActionEvent event) -> {
 			try {
 				
+				// COMPROBATIONS ARE ALREADY DONE IN FXML FUNCTIONS
+				
 				LaunchClientApp.instruction = ("search_user_by_id, " + logInController.user_id);
-				user_id = Integer.parseInt(LaunchClientApp.feedback);
-				
-			// QUEDA VERIFICAR SI EL NUEVO NOMBRE DE USUARIO NO EXISTE !!
-			
-				//LaunchClientApp.instruction = ("change_user_info, " + passwordField.getText() + ", " + emailField.getText() + ", " + logInController.user_id);
-				//user_id = Integer.parseInt(LaunchClientApp.feedback);
-				
-				LaunchClientApp.dataOutputStream.writeUTF("change_user_info, " + passwordField.getText() + ", " + emailField.getText() + ", " + logInController.user_id);
+				LaunchClientApp.dataOutputStream.writeUTF(LaunchClientApp.instruction);
 				user_id = Integer.parseInt(LaunchClientApp.dataInputStream.readUTF());
+				System.out.println(user_id);
+				
+				LaunchClientApp.dataOutputStream.writeUTF("change_user_info, " + passwordField.getText() + "," + emailField.getText() + "," + logInController.user_id);
+				System.out.println("User updated " + LaunchClientApp.dataInputStream.readUTF());
 				
 				Pane home_pane_fxml = FXMLLoader.load(getClass().getResource("HomeView.fxml"));
 				mainPane.getChildren().removeAll();
 				mainPane.getChildren().setAll(home_pane_fxml);
-				
-				
+						
 			
 			} catch (IOException home_error) {
 				home_error.printStackTrace();
@@ -75,18 +79,44 @@ public class SettingsController implements Initializable {
 	}
 
 	@FXML
-	private void validate_password(MouseEvent event) {
+	void validate_password(MouseEvent event) {
 		
 		if(!passwordField.getText().equals(null) & !confirmPasswordField.getText().equals(null) & passwordField.getText().equals(confirmPasswordField.getText())) {
 			okayVerification.setVisible(true);
+			warningText.setText("ERROR! The password must match");
 			warning.setVisible(false);
 			saveButton.setDisable(false);
+			okayMessage.setVisible(false);
 			
 		} else {
 			saveButton.setDisable(true);
 			okayVerification.setVisible(false);
 			warning.setVisible(true);
+			okayMessage.setVisible(false);
 		}
 		
 	}
+	
+	@FXML
+	void validate_email(MouseEvent event) throws IOException {
+		if(!emailField.getText().equals("")) {
+			
+			LaunchClientApp.dataOutputStream.writeUTF("search_existent_email,"+emailField.getText());
+			
+			if (LaunchClientApp.dataInputStream.readUTF().equals("wrong")) {
+				warning.setVisible(true);
+				warningText.setText("Email already exists!");
+				saveButton.setDisable(true);
+				okayMessage.setVisible(false);
+			} else {
+				okayMessage.setVisible(true);
+				warning.setVisible(false);
+			}
+			
+		} else {
+			okayMessage.setVisible(false);
+			warning.setVisible(false);
+		}
+	}
+	
 }
